49d1a2bb30066a73640e84c283ed9effa4eabec4
xerxes <xerxeslowjiejun@gmail.com>
2023-02-07 21:45:44 +0800

    geography and geometry progress

--- Quizzes/Geography.js
+++ Quizzes/Geography.js
@@ -1,43 +1,43 @@
 function checkAnswers() {
     var score = 0;
-    var answer1 = document.querySelector('input[name="history1"]:checked').value;
+    var answer1 = document.querySelector('input[name="g1"]:checked').value;
     if (answer1 === "Paris") {
       score += 1;
     }
-    var answer2 = document.querySelector('input[name="history2"]:checked').value;
-    if (answer2 === "Yen") {
+    var answer2 = document.querySelector('input[name="g2"]:checked').value;
+    if (answer2 === "Indian Ocean") {
       score += 1;
     }
-    var answer3 = document.querySelector('input[name="history3"]:checked').value;
-    if (answer3 === "Yen") {
+    var answer3 = document.querySelector('input[name="g3"]:checked').value;
+    if (answer3 === "Canada") {
       score += 1;
     }
-    var answer4 = document.querySelector('input[name="history4"]:checked').value;
-    if (answer4 === "Yen") {
+    var answer4 = document.querySelector('input[name="g4"]:checked').value;
+    if (answer4 === "Canberra") {
       score += 1;
     }
-    var answer5 = document.querySelector('input[name="history5"]:checked').value;
-    if (answer5 === "Yen") {
+    var answer5 = document.querySelector('input[name="g5"]:checked').value;
+    if (answer5 === "Sahara Desert") {
       score += 1;
     }
-    var answer6 = document.querySelector('input[name="history6"]:checked').value;
-    if (answer6 === "Yen") {
+    var answer6 = document.querySelector('input[name="g6"]:checked').value;
+    if (answer6 === "Mississippi River") {
       score += 1;
     }
-    var answer7 = document.querySelector('input[name="history7"]:checked').value;
-    if (answer7 === "Yen") {
+    var answer7 = document.querySelector('input[name="g7"]:checked').value;
+    if (answer7 === "Brasília") {
       score += 1;
     }
-    var answer8 = document.querySelector('input[name="history8"]:checked').value;
-    if (answer8 === "Yen") {
+    var answer8 = document.querySelector('input[name="g8"]:checked').value;
+    if (answer8 === "Beijing") {
       score += 1;
     }
-    var answer9 = document.querySelector('input[name="history9"]:checked').value;
-    if (answer9 === "Yen") {
+    var answer9 = document.querySelector('input[name="g9"]:checked').value;
+    if (answer9 === "Mexico") {
       score += 1;
     }
-    var answer10 = document.querySelector('input[name="history10"]:checked').value;
-    if (answer10 === "Yen") {
+    var answer10 = document.querySelector('input[name="g10"]:checked').value;
+    if (answer10 === "Mount Everest") {
       score += 1;
     }
     document.getElementById("result").innerHTML = "Your total score is " + score + " out of 10.";
--- Quizzes/GeometryAns.js
+++ Quizzes/GeometryAns.js
@@ -1,43 +1,43 @@
 function checkAnswers() {
     var score = 0;
-    var answer1 = document.querySelector('input[name="history1"]:checked').value;
-    if (answer1 === "Paris") {
+    var answer1 = document.querySelector('input[name="gm1"]:checked').value;
+    if (answer1 === "A = bh") {
       score += 1;
     }
-    var answer2 = document.querySelector('input[name="history2"]:checked').value;
-    if (answer2 === "Yen") {
+    var answer2 = document.querySelector('input[name="gm2"]:checked').value;
+    if (answer2 === "P = 4s") {
       score += 1;
     }
-    var answer3 = document.querySelector('input[name="history3"]:checked').value;
-    if (answer3 === "Yen") {
+    var answer3 = document.querySelector('input[name="gm3"]:checked').value;
+    if (answer3 === "A part of a line that has two endpoints") {
       score += 1;
     }
-    var answer4 = document.querySelector('input[name="history4"]:checked').value;
-    if (answer4 === "Yen") {
+    var answer4 = document.querySelector('input[name="gm4"]:checked').value;
+    if (answer4 === "V = πr^2h") {
       score += 1;
     }
-    var answer5 = document.querySelector('input[name="history5"]:checked').value;
-    if (answer5 === "Yen") {
+    var answer5 = document.querySelector('input[name="gm5"]:checked').value;
+    if (answer5 === "C = 2πr") {
       score += 1;
     }
-    var answer6 = document.querySelector('input[name="history6"]:checked').value;
-    if (answer6 === "Yen") {
+    var answer6 = document.querySelector('input[name="gm6"]:checked').value;
+    if (answer6 === "A line that cuts another line into two equal parts at a 90-degree angle") {
       score += 1;
     }
-    var answer7 = document.querySelector('input[name="history7"]:checked').value;
-    if (answer7 === "Yen") {
+    var answer7 = document.querySelector('input[name="gm7"]:checked').value;
+    if (answer7 === "A = πr^2") {
       score += 1;
     }
-    var answer8 = document.querySelector('input[name="history8"]:checked').value;
-    if (answer8 === "Yen") {
+    var answer8 = document.querySelector('input[name="gm8"]:checked').value;
+    if (answer8 === "A closed figure with three or more straight sides") {
       score += 1;
     }
-    var answer9 = document.querySelector('input[name="history9"]:checked').value;
-    if (answer9 === "Yen") {
+    var answer9 = document.querySelector('input[name="gm9"]:checked').value;
+    if (answer9 === "An angle that measures exactly 90 degrees") {
       score += 1;
     }
-    var answer10 = document.querySelector('input[name="history10"]:checked').value;
-    if (answer10 === "Yen") {
+    var answer10 = document.querySelector('input[name="gm10"]:checked').value;
+    if (answer10 === "V = lwh") {
       score += 1;
     }
     document.getElementById("result").innerHTML = "Your total score is " + score + " out of 10.";
