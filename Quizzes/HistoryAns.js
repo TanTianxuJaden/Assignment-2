13a6f22d73950f8d25d2a799e2500f4de7393898
TanTianxuJaden <s10242805@connect.np.edu.sg>
2023-02-07 19:04:58 +0800

    Progress on mcq - Jaden

--- Quizzes/HistoryAns.js
+++ Quizzes/HistoryAns.js
@@ -1,10 +1,10 @@
 function checkAnswers() {
     var score = 0;
-    var answer1 = document.querySelector('input[name="answer1"]:checked').value;
+    var answer1 = document.querySelector('input[name="history1"]:checked').value;
     if (answer1 === "Paris") {
       score += 1;
     }
-    var answer2 = document.querySelector('input[name="answer2"]:checked').value;
+    var answer2 = document.querySelector('input[name="history2"]:checked').value;
     if (answer2 === "Yen") {
       score += 1;
     }
