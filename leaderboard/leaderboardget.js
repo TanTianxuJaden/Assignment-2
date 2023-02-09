48f112051985eed5dbd6766231c538a7c11f05f5
xerxes <xerxeslowjiejun@gmail.com>
2023-02-09 18:17:15 +0800

    leaderboard api progress

---
+++ leaderboard/leaderboardget.js
@@ -0,0 +1,28 @@
+let apikey = "63d663b83bc6b255ed0c43f7";
+
+var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://assignment2-1813.restdb.io/rest/leaderboard",
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": apikey,
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    let leaderboardTable = document.getElementById("leaderboard-table");
+    response.forEach(function(item) {
+      let tr = document.createElement("tr");
+      let scoreTd = document.createElement("td");
+      scoreTd.innerText = item.score;
+      tr.appendChild(scoreTd);
+      let subjectTd = document.createElement("td");
+      subjectTd.innerText = item.subject;
+      tr.appendChild(subjectTd);
+      leaderboardTable.appendChild(tr);
+    });
+  });
